3fb0f4368957d848a65fed729a3a77f97d244e43
Jeffrey <tkdbb84@gmail.com>
2018-01-20 10:13:21 -0500

    recall messages

--- sassybot.js
+++ sassybot.js
@@ -5,15 +5,16 @@ const db = new sqlite3.Database('/home/nodebot/data/nodebot.sqlite');
 const fs = require('fs');
 
 let getSecrets = () => {
-    return JSON.parse(fs.readFileSync("client_secrets.json"));
+    return JSON.parse(fs.readFileSync("/home/nodebot/src/client_secrets.json"));
 };
-let addRow, removeRow, addQuote, getQuotesByUser;
+let channelList = new Map();
+let addSpamChannel, removeSpamChannel, addQuote, getQuotesByUser;
 
 
 client.on('ready', () => {
     console.log('I am ready!');
     db.exec('CREATE TABLE IF NOT EXISTS spam_channels (guild_id TEXT PRIMARY KEY, channel_id TEXT) WITHOUT ROWID;');
-    db.exec('CREATE TABLE IF NOT EXISTS user_quotes (guild_id, user_id TEXT, message_id TEXT, timestamp INTEGER);');
+    db.exec('CREATE TABLE IF NOT EXISTS user_quotes (guild_id TEXT, user_id TEXT, message_id TEXT, timestamp INTEGER);');
     db.all('SELECT * FROM spam_channels', (error, rows) => {
         if(!error) {
             rows.forEach((row) => {
@@ -21,8 +22,8 @@ client.on('ready', () => {
             });
         }
     });
-    addRow = db.prepare('INSERT INTO spam_channels (guild_id, channel_id) VALUES (?,?);');
-    removeRow = db.prepare('DELETE FROM spam_channels WHERE guild_id = ?;');
+    addSpamChannel = db.prepare('INSERT INTO spam_channels (guild_id, channel_id) VALUES (?,?);');
+    removeSpamChannel = db.prepare('DELETE FROM spam_channels WHERE guild_id = ?;');
     addQuote = db.prepare('INSERT INTO user_quotes (guild_id, user_id, message_id, timestamp) VALUES (?,?,?,strftime(\'%s\',\'now\'));');
     getQuotesByUser = db.prepare('SELECT * FROM user_quotes WHERE guild_id = ? AND user_id = ?');
 });
@@ -36,8 +37,8 @@ let chatFunctions = {
     },
     'spam': (message) => {
         channelList.set(message.guild.id, message.channel.id);
-        removeRow.run([message.guild.id]);
-        addRow.run([message.guild.id, message.channel.id]);
+        removeSpamChannel.run([message.guild.id]);
+        addSpamChannel.run([message.guild.id, message.channel.id]);
         message.reply('Ok, I\'ll spam this channel');
     },
     'rquote': (message) => {
@@ -47,19 +48,76 @@ let chatFunctions = {
                 if(!error && rows.length > 0) {
                     let row = rows[Math.floor(Math.random() * rows.length)];
                     message.channel.fetchMessage(row.message_id).then((recalledMessage) => {
-                        let content = recalledMessage.cleanContent();
-                        let attachment = {};
-                        let embeds = [];
-                        if ( recalledMessage.attachments && recalledMessage.attachments.array().length > 0 ) {
-
-                        }
-
+                        let content = recalledMessage.cleanContent;
+                        let embed = null;
                         if ( recalledMessage.embeds && recalledMessage.embeds.length > 0 ) {
-
+                            // discord JS can only deal w/ 1 embed at a time
+                            /** @var MessageEmbed tmp */
+                            let tmp = recalledMessage.embeds[0];
+                            embed = {
+                                title: tmp.title,
+                                type: tmp.type,
+                                description: tmp.description,
+                                url: tmp.url,
+                                timestamp: tmp.createdTimestamp,
+                                color: tmp.color,
+                                footer: tmp.footer ? {
+                                    'text': tmp.footer.text,
+                                    'icon_url': tmp.footer.iconURL,
+                                    'proxy_icon_url': tmp.footer.proxyIconUrl
+                                } : {},
+                                image: tmp.image ? {
+                                    'url': tmp.image.url,
+                                    'proxy_url': tmp.image.proxyURL,
+                                    'height': tmp.image.height,
+                                    'width': tmp.image.width
+                                } : {},
+                                thumbnail: tmp.thumbnail ? {
+                                    'url': tmp.thumbnail.url,
+                                    'proxy_url': tmp.thumbnail.proxyURL,
+                                    'height': tmp.thumbnail.height,
+                                    'width': tmp.thumbnail.width
+                                } : {},
+                                video: tmp.video ? {
+                                    'url': tmp.video.url,
+                                    'height': tmp.video.height,
+                                    'width': tmp.video.width
+                                } : {},
+                                provider: tmp.provider ? {
+                                    'name': tmp.provider.name,
+                                    'url': tmp.provider.url
+                                } : {},
+                                author: tmp.author ? {
+                                    'name': tmp.author.name,
+                                    'url': tmp.author.url,
+                                    'icon_url': tmp.author.iconURL
+                                } : {},
+                                fields: (() => {
+                                    let result = [];
+                                    for (let i = 0, iMax = tmp.fields.length ; i < iMax ; i++) {
+                                        result.push(
+                                            tmp[i].fields.map(
+                                                (item) => {
+                                                    return {
+                                                        'inline': item.inline,
+                                                        'name': item.name,
+                                                        'value': item.value
+                                                    }
+                                                }
+                                            )
+                                        );
+                                    }
+                                    return result;
+                                })()
+                            }
                         }
+                        messageOptions = {
+                            disableEveryone: true,
+                            embed: embed ? embed : {}
+                        };
+                        message.channel.send(quotedMember.displayName + ' said: "' + content + '"', messageOptions);
+                        message.channel.send('and has ' + ((rows.length - 1) === 0 ? 'No' : (rows.length - 1))  + ' other quotes saved');
                     });
-                    // message.channel.send(quotedMember.displayName + ' said: "' + row.quote + '"');
-                    // message.channel.send(quotedMember.displayName + ' has ' + ((row.length - 1) === 0 ? 'No' : (row.length - 1))  + ' other quotes saved');
                 }
             });
         } else {
@@ -83,8 +141,8 @@ let chatFunctions = {
                                     reaction.fetchUsers().then(
                                         (users) => {
                                             if (users.get(message.author.id) && !foundOne) {
-                                                // addQuote.run([message.guild.id, reaction.message.author.id, reaction.message.id]);
-                                                message.reply(' ' + 'I\'ve noted that ' + reaction.message.author.displayName + ' said: "' + reaction.message.cleanContent +  '"');
+                                                addQuote.run([message.guild.id, reaction.message.author.id, reaction.message.id]);
+                                                message.reply(' ' + 'I\'ve noted that ' + quotedMember.displayName + ' said: "' + reaction.message.cleanContent +  '"');
                                                 foundOne = true;
                                             }
                                         }
