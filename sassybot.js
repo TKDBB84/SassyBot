b88aafeadeef84eccae3f00ab0c2f29fc6be72df
Jeffrey <tkdbb84@gmail.com>
2018-08-04 22:01:26 -0400

    go back to store quotes by text only

--- sassybot.js
+++ sassybot.js
@@ -8,13 +8,14 @@ let getSecrets = () => {
     return JSON.parse(fs.readFileSync("/home/nodebot/src/client_secrets.json"));
 };
 let channelList = new Map();
-let addSpamChannel, removeSpamChannel, addQuote, getQuotesByUser;
+let addSpamChannel, removeSpamChannel, addQuote, getQuotesByUser,
+    getQuoteCountByUser, updateMesageText;
 
 
 client.on('ready', () => {
     console.log('I am ready!');
     db.exec('CREATE TABLE IF NOT EXISTS spam_channels (guild_id TEXT PRIMARY KEY, channel_id TEXT) WITHOUT ROWID;');
-    db.exec('CREATE TABLE IF NOT EXISTS user_quotes (guild_id TEXT, user_id TEXT, channel_id TEXT, message_id TEXT, timestamp INTEGER);');
+    db.exec('CREATE TABLE IF NOT EXISTS user_quotes (guild_id TEXT, user_id TEXT, channel_id TEXT, message_id TEXT, timestamp INTEGER, quote_text TEXT);');
     db.all('SELECT * FROM spam_channels', (error, rows) => {
         if(!error) {
             rows.forEach((row) => {
@@ -24,12 +25,16 @@ client.on('ready', () => {
     });
     addSpamChannel = db.prepare('INSERT INTO spam_channels (guild_id, channel_id) VALUES (?,?);');
     removeSpamChannel = db.prepare('DELETE FROM spam_channels WHERE guild_id = ?;');
-    addQuote = db.prepare('INSERT INTO user_quotes (guild_id, user_id, channel_id, message_id, timestamp) VALUES (?,?,?,?,strftime(\'%s\',\'now\'));');
-    getQuotesByUser = db.prepare('SELECT * FROM user_quotes WHERE guild_id = ? AND user_id = ?');
+    addQuote = db.prepare('INSERT INTO user_quotes (guild_id, user_id, channel_id, message_id, timestamp, quote_text) VALUES (?,?,?,?,strftime(\'%s\',\'now\'),?);');
+    getQuotesByUser = db.prepare('SELECT * FROM user_quotes WHERE guild_id = ? AND user_id = ? ORDER BY message_id;');
+    getQuoteCountByUser = db.prepare('SELECT COUNT(1) as cnt FROM user_quotes WHERE guild_id = ? AND user_id = ?;');
+    updateMesageText = db.prepare('UPDATE user_quotes SET quote_text = ? WHERE message_id = ?;');
+
 });
 
 let chatFunctions = {
     'ping': (message) => {
+        console.log('sassybot pinged');
         message.reply('pong');
     },
     'echo': (message) => {
@@ -46,91 +51,24 @@ let chatFunctions = {
             let quotedMember = message.mentions.members.first();
             getQuotesByUser.all([message.guild.id, quotedMember.id], (error, rows) => {
                 if(!error && rows.length > 0) {
-                    let row = rows[Math.floor(Math.random() * rows.length)];
-                    client.channels.get(row.channel_id).fetchMessage(row.message_id).then((recalledMessage) => {
-                        let content = recalledMessage.cleanContent;
-                        let embed = null;
-                        let attachment = null;
-                        if ( recalledMessage.attachments.array().length > 0 ) {
-                            let tmpAttachment = recalledMessage.attachments.first();
-                            attachment = new Discord.Attachment(tmpAttachment.url, tmpAttachment.filename);
-                        }
-                        if ( !attachment && recalledMessage.embeds && recalledMessage.embeds.length > 0 ) {
-                            // discord JS can only deal w/ 1 embed at a time
-                            /** @var MessageEmbed tmp */
-                            let tmp = recalledMessage.embeds[0];
-                            embed = {
-                                title: tmp.title,
-                                type: tmp.type,
-                                description: tmp.description,
-                                url: tmp.url,
-                                timestamp: tmp.createdTimestamp,
-                                color: tmp.color,
-                                footer: tmp.footer ? {
-                                    'text': tmp.footer.text,
-                                    'icon_url': tmp.footer.iconURL,
-                                    'proxy_icon_url': tmp.footer.proxyIconUrl
-                                } : {},
-                                image: tmp.image ? {
-                                    'url': tmp.image.url,
-                                    'proxy_url': tmp.image.proxyURL,
-                                    'height': tmp.image.height,
-                                    'width': tmp.image.width
-                                } : {},
-                                thumbnail: tmp.thumbnail ? {
-                                    'url': tmp.thumbnail.url,
-                                    'proxy_url': tmp.thumbnail.proxyURL,
-                                    'height': tmp.thumbnail.height,
-                                    'width': tmp.thumbnail.width
-                                } : {},
-                                video: tmp.video ? {
-                                    'url': tmp.video.url,
-                                    'height': tmp.video.height,
-                                    'width': tmp.video.width
-                                } : {},
-                                provider: tmp.provider ? {
-                                    'name': tmp.provider.name,
-                                    'url': tmp.provider.url
-                                } : {},
-                                author: tmp.author ? {
-                                    'name': tmp.author.name,
-                                    'url': tmp.author.url,
-                                    'icon_url': tmp.author.iconURL
-                                } : {},
-                                fields: (() => {
-                                    let result = [];
-                                    for (let i = 0, iMax = tmp.fields.length ; i < iMax ; i++) {
-                                        result.push(
-                                            tmp[i].fields.map(
-                                                (item) => {
-                                                    return {
-                                                        'inline': item.inline,
-                                                        'name': item.name,
-                                                        'value': item.value
-                                                    }
-                                                }
-                                            )
-                                        );
-                                    }
-                                    return result;
-                                })()
-                            }
-                        }
-                        messageOptions = {
-                            disableEveryone: true,
-                            embed: embed ? embed : {}
-                        };
-                        if (attachment) {
-                            message.channel.send(quotedMember.displayName + ' said: "' + content + '"', attachment);
-                        } else {
-                            message.channel.send(quotedMember.displayName + ' said: "' + content + '"', messageOptions);
-                        }
-                        message.channel.send('and has ' + ((rows.length - 1) === 0 ? 'No' : (rows.length - 1))  + ' other quotes saved');
-                    });
+                    let selectedQuoted = Math.floor(Math.random() * rows.length);
+                    let row = rows[selectedQuoted];
+                    selectedQuoted += 1;
+                    if (row.quote_text && row.quote_text !== '') {
+                        message.channel.send(quotedMember.displayName + ' said: "' + content + '" (quote #' + selectedQuoted + ')', {disableEveryone: true});
+                        message.channel.send('and has ' + ((rows.length - 1) === 0 ? 'No' : (rows.length - 1)) + ' other quotes saved');
+                    } else {
+                        client.channels.get(row.channel_id).fetchMessage(row.message_id).then((recalledMessage) => {
+                            let content = recalledMessage.cleanContent;
+                            message.channel.send(quotedMember.displayName + ' said: "' + content + '" (quote #' + selectedQuoted + ')', {disableEveryone: true});
+                            message.channel.send('and has ' + ((rows.length - 1) === 0 ? 'No' : (rows.length - 1)) + ' other quotes saved');
+                            updateMesageText.run([row.message_id, content]);
+                        });
+                    }
                 }
             });
         } else {
-
+            message.channel.send('You must specify who\'s quote you want to retrieve', {disableEveryone: true});
         }
     },
     'quote': (message) => {
@@ -150,7 +88,7 @@ let chatFunctions = {
                                     reaction.fetchUsers().then(
                                         (users) => {
                                             if (users.get(message.author.id) && !foundOne) {
-                                                addQuote.run([message.guild.id, reaction.message.author.id, activeChannel.id, reaction.message.id]);
+                                                addQuote.run([message.guild.id, reaction.message.author.id, activeChannel.id, reaction.message.id, reaction.message.cleanContent]);
                                                 message.reply(' ' + 'I\'ve noted that ' + quotedMember.displayName + ' said: "' + reaction.message.cleanContent +  '"');
                                                 foundOne = true;
                                             }
@@ -177,24 +115,25 @@ let chatFunctions = {
             }
             message.reply(' ' + JSON.stringify(diceRolls) + ' => ' + diceRolls.reduce((total, num)=> total + num))
         }
-    }
+    }//, 'purge': (message) => { message.reply('ok: commencing a purge...'); }
 };
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
+    let now = '(' + (new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')) + ' GMT) ';
     if (oldMember.voiceChannelID !== newMember.voiceChannelID) {
         if (oldMember.voiceChannelID && !newMember.voiceChannelID && channelList.has(oldMember.guild.id)) {
             let leftChannel = client.channels.get(oldMember.voiceChannelID);
-            let msg = oldMember.displayName + ' has left ' + leftChannel.name;
+            let msg = now + oldMember.displayName + ' has left ' + leftChannel.name;
             client.channels.get(channelList.get(oldMember.guild.id)).send(msg);
         } else if (!oldMember.voiceChannelID && newMember.voiceChannelID && channelList.has(newMember.guild.id)) {
             let joinedChannel = client.channels.get(newMember.voiceChannelID);
-            let msg = oldMember.displayName + ' has joined ' + joinedChannel.name;
+            let msg = now + oldMember.displayName + ' has joined ' + joinedChannel.name;
             client.channels.get(channelList.get(joinedChannel.guild.id)).send(msg);
         } else {
             if (channelList.has(oldMember.guild.id)) {
                 let leftChannel = client.channels.get(oldMember.voiceChannelID);
                 let joinedChannel = client.channels.get(newMember.voiceChannelID);
-                let msg = oldMember.displayName + ' has moved from: ' + leftChannel.name + ' to: ' + joinedChannel.name;
+                let msg = now + oldMember.displayName + ' has moved from: ' + leftChannel.name + ' to: ' + joinedChannel.name;
                 client.channels.get(channelList.get(oldMember.guild.id)).send(msg);
             }
         }
