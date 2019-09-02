97f0460ed3aadb2b91f70ed23bd6c74ff6f9909b
tkdbb84 <tkdbb84@gmail.com>
2019-09-02 15:13:03 -0700

    record members from XIVAPI

--- src/CoTMembers.ts
+++ src/CoTMembers.ts
@@ -1,4 +1,5 @@
-// import * as fs from 'fs';
+import * as fs from 'fs';
+import * as http2 from 'http2';
 import SassyDb from './SassyDb';
 import { User } from './Users';
 
@@ -9,32 +10,22 @@ interface IMemberRow {
   first_seen_discord: number;
   last_promotion: number;
 }
+const ONE_HOUR = 3600000;
 
-// export interface IFreeCompanyMember {
-//   Avatar: 'https://img2.finalfantasyxiv.com/f/9bb002c4984cb609a79dd28c4079c5d4_ce736afe35e2ded4e46c4fd0659aef7efc0_96x96.jpg';
-//   FeastMatches: number;
-//   ID: number;
-//   Name: string;
-//   Rank: string;
-//   RankIcon: string;
-//   Server: string;
-// }
-//
-// interface IClientSecrets {
-//   token: string;
-//   xivApiToken: string;
-// }
-//
-// const getSecrets: () => IClientSecrets = (): IClientSecrets => {
-//   const fileData = fs.readFileSync('/home/nodebot/src/client_secrets.json');
-//   return JSON.parse(fileData.toString());
-// };
-//
-// // tslint:disable-next-line:no-var-requires
-// const XIVApi = require('xivapi-js');
-// export const xivClient = new XIVApi({
-//   private_key: getSecrets().xivApiToken,
-// });
+export interface IFreeCompanyMember {
+  Avatar: 'https://img2.finalfantasyxiv.com/f/9bb002c4984cb609a79dd28c4079c5d4_ce736afe35e2ded4e46c4fd0659aef7efc0_96x96.jpg';
+  FeastMatches: number;
+  ID: number;
+  Name: string;
+  Rank: string;
+  RankIcon: string;
+  Server: string;
+}
+
+interface IClientSecrets {
+  token: string;
+  xivApiToken: string;
+}
 
 const db = new SassyDb();
 db.connection.exec(
@@ -44,8 +35,56 @@ db.connection.exec(
 db.connection.exec('DROP TABLE IF EXISTS cot_member');
 
 db.connection.exec(
-  'CREATE TABLE IF NOT EXISTS cot_members (api_id TEXT PRIMARY KEY, user_id TEXT, name TEXT, rank TEXT, first_seen_api TIMESTAMP DEFAULT CURRENT_TIMESTAMP);',
+  'CREATE TABLE IF NOT EXISTS cot_members (api_id TEXT PRIMARY KEY, user_id TEXT, name TEXT, rank TEXT, first_seen_api TIMESTAMP DEFAULT CURRENT_TIMESTAMP, last_seen_api TIMESTAMP DEFAULT CURRENT_TIMESTAMP);',
 );
+
+type upsertMemberFunction = ({ ID, Name, Rank }: IFreeCompanyMember) => boolean;
+const upsertMember: upsertMemberFunction = ({ ID, Name, Rank }) => {
+  const stmtUpsertMember = db.connection.prepare("INSERT INTO cot_members (api_id, user_id, name, rank) VALUES (?, '', ?, ?) ON CONFLICT(api_id) DO UPDATE SET rank = ?, last_seen_api = CURRENT_TIMESTAMP");
+  const result = stmtUpsertMember.run([ID, Name, Rank, Rank]);
+  return !!result.lastInsertRowid || !!result.changes
+};
+
+const getSecrets: () => IClientSecrets = (): IClientSecrets => {
+  const fileData = fs.readFileSync('/home/nodebot/src/client_secrets.json');
+  return JSON.parse(fileData.toString());
+};
+
+const getLatestMemberList = (): Promise<IFreeCompanyMember[]> => {
+  return new Promise((resolve) => {
+    const client = http2.connect('https://xivapi.com:443');
+    client.on('error', console.error);
+    const req = client.request({
+      ':path': `/freecompany/9229001536389012456?private_key=${getSecrets().xivApiToken}&data=FCM`,
+    });
+    req.setEncoding('utf8');
+    let responseBody = '';
+    req.on('data', (chunk) => {
+      responseBody += chunk;
+    });
+    req.on('end', () => {
+      let finalResult: IFreeCompanyMember[] = [];
+      try {
+        const parsedBody = JSON.parse(responseBody);
+        if (parsedBody.hasOwnProperty('FreeCompanyMembers')) {
+          finalResult = parsedBody.FreeCompanyMembers;
+        }
+      } catch (err) {
+        console.error({ context: 'Error From XIVAPI', err });
+        finalResult = [];
+      }
+      client.close(() => {
+        resolve(finalResult);
+      });
+    });
+    req.end();
+  });
+};
+
+setInterval(async () => {
+  const currentMembers = await getLatestMemberList();
+  currentMembers.map(upsertMember);
+}, ONE_HOUR * 12);
 
 type AddMemberFunction = ({ userId, name }: { userId: string; name: string }) => boolean;
 const addMember: AddMemberFunction = ({ userId, name }) => {
