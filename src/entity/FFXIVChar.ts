0780df8915b73a4dbb4bb4b942e428f82c2483e7
Jeffrey Campbell <jeffrey.campbell@buildingconnected.com>
2020-02-07 19:18:59 -0800

    add charset explicitly

--- src/entity/FFXIVChar.ts
+++ src/entity/FFXIVChar.ts
@@ -9,7 +9,7 @@ export default class FFXIVChar {
   @Column()
   public apiId!: number;
 
-  @Column()
+  @Column( { charset: "utf8", collation: "utf8_general_ci"})
   public name!: string;
 
   @CreateDateColumn()
