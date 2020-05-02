0990a71bcceb3535c5695edec4c72170cc70589b
tkdbb84 <tkdbb84@gmail.com>
2020-05-01 17:05:52 -0700

    there we go

---
+++ src/sassybotEventListeners/BussyListener.ts
@@ -0,0 +1,20 @@
+import { Message } from 'discord.js';
+import { GuildIds } from '../consts';
+import SassybotEventListener from './SassybotEventListener';
+
+export default class BussyListener extends SassybotEventListener {
+  public readonly event = 'messageReceived';
+
+  public getEventListener() {
+    return this.listener.bind(this);
+  }
+
+  protected async listener({ message }: { message: Message }): Promise<void> {
+    if (message.author.id === '153364394443669507' && message.guild && message.guild.id === GuildIds.COT_GUILD_ID) {
+      const bussy = await this.sb.getMember(GuildIds.COT_GUILD_ID, message.author.id);
+      if (Math.random() >= 0.8) {
+        await message.channel.send(`Hi, I\'d just like to remind everyone that ${bussy} wanted a bot to rate his cock`);
+      }
+    }
+  }
+}
--- src/sassybotEventListeners/sassybotCommands/CockCommand.ts
+++ src/sassybotEventListeners/sassybotCommands/CockCommand.ts
@@ -10,12 +10,18 @@ export default class EchoCommand extends SassybotCommand {
   }
 
   protected async listener({ message, params }: { message: Message; params: ISassybotCommandParams }): Promise<void> {
-    let resp = '10/10, you have a perfect & beautiful cock.'
+    let resp = '10/10, '
     if (Math.random() > 0.5) {
-      resp = '10/10, that thing really turns my circuits on.'
+      resp += 'that thing really turns my circuits on.'
+    } else if (Math.random() > 0.5) {
+      resp += 'wow: I didn\'t expect it to be so big'
+    } else if (Math.random() > 0.5) {
+      resp += ''
+    } else {
+      resp += 'you have a perfect & beautiful cock.'
     }
     if (message.author.id === '125025069402554368') {
-      resp = '10/10, i want it to rub on my soft furry programming!`'
+      resp = '10/10, i want it to rub on my soft furry programming!'
     }
     if (message.author.id === '153364394443669507') {
       resp = 'holy shit, WTF is wrong with that thing, go see a doctor... for real.'
