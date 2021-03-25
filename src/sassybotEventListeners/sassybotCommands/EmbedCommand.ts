7ca6011906c7d116e2d3b7368cddc83addff86f2
Jeffrey Campbell <jeffrey.campbell@autodesk.com>
2021-03-25 15:51:51 -0700

    wip

--- src/sassybotEventListeners/sassybotCommands/EmbedCommand.ts
+++ src/sassybotEventListeners/sassybotCommands/EmbedCommand.ts
@@ -19,18 +19,18 @@ export default class EmbedCommand extends SassybotCommand {
     const xiv = new XIVApi({ private_key: process.env.XIV_API_TOKEN, language: 'en' });
     const declaredName = 'sasner rensas'
     const {Results}: XIVAPISearchResponse = await xiv.character.search(declaredName, {server: 'Jenova'})
-    const apiChars = Results.filter(result => result.Name.trim().toLowerCase() !== declaredName.toLowerCase())
-    // if (apiChars.length === 1) {
-    const apiCharacter = apiChars[0]
-    const embed = new MessageEmbed({
-      title: 'This You?',
-      description: 'Is this your character?',
-      image: {url: apiCharacter.Avatar},
-      footer: {
-        text: 'Yes/No'
-      }
-    })
-    await message.reply(embed)
-    // }
+    const apiChars = Results.filter(result => result.Name.trim().toLowerCase() === declaredName.toLowerCase())
+    if (apiChars.length === 1) {
+      const apiCharacter = apiChars[0]
+      const embed = new MessageEmbed({
+        title: 'This You?',
+        description: 'Is this your character?',
+        image: {url: apiCharacter.Avatar},
+        footer: {
+          text: 'Yes/No'
+        }
+      })
+      await message.reply(embed)
+    }
   }
 }
