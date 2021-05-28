677c933c059ceb07a235fc46ceaf4b9d4fc3dd04
Jeffrey Campbell <jeffrey.campbell@autodesk.com>
2021-05-28 11:31:56 -0700

    need to fix discord transport first

--- src/log.ts
+++ src/log.ts
@@ -1,15 +1,15 @@
 import * as winston from 'winston';
-import DiscordTransport from 'winston-discord-transport';
+// import DiscordTransport from 'winston-discord-transport';
 
 let winLogger;
 const config = {
   exceptionHandlers: [
-    new DiscordTransport({
-      webhook:
-        'https://discord.com/api/webhooks/367794012406415360/e_L9nHvvj1_nZYN1sb3pKsk0p2nP_EwE4bMnAlWCMkTotmTAU4R6rrw7H5KJNQT-EpI3',
-      defaultMeta: { service: 'Sassybot' },
-      level: 'error',
-    }),
+    // new DiscordTransport({
+    //   webhook:
+    //     'https://discord.com/api/webhooks/367794012406415360/e_L9nHvvj1_nZYN1sb3pKsk0p2nP_EwE4bMnAlWCMkTotmTAU4R6rrw7H5KJNQT-EpI3',
+    //   defaultMeta: { service: 'Sassybot' },
+    //   level: 'error',
+    // }),
     new winston.transports.Console({
       format: winston.format.combine(
         winston.format.timestamp(),
