287b51ab38eea6e4bf9398d960f965518e4936e6
Jeffrey Campbell <jeffrey.campbell@autodesk.com>
2021-05-30 15:46:25 -0700

    maybe?

--- src/log.ts
+++ src/log.ts
@@ -42,16 +42,40 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 export function createLogger(discordClient: Client) {
-  config.transports.push(
-    new DiscordTransport({
-      discordClient,
-      discordChannel: '848648942740963338',
-    }),
-  );
+  const withDiscord: any = {
+    exceptionHandlers: [
+      new winston.transports.Console({
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          winston.format.colorize(),
+          winston.format.splat(),
+          winston.format.simple(),
+        ),
+        level: 'error',
+        stderrLevels: ['error'],
+      }),
+    ],
+    format: winston.format.combine(winston.format.timestamp(), winston.format.simple()),
+    transports: [
+      new DiscordTransport({
+        discordClient,
+        discordChannel: '848648942740963338',
+      }),
+      new winston.transports.Console({
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          winston.format.colorize(),
+          winston.format.splat(),
+          winston.format.simple(),
+        ),
+        stderrLevels: ['error'],
+      }),
+    ],
+  };
   if (process.env.NODE_ENV === 'production') {
     return winston.createLogger({
       level: 'info',
-      ...config,
+      ...withDiscord,
     });
   }
   return winston.createLogger({
