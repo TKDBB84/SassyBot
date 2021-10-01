3e11378c44de0db707d452826f19d6c320abe991
Jeffrey Campbell <jeffrey.campbell@autodesk.com>
2021-10-01 12:52:12 -0700

    build

--- src/sassybotEventListeners/sassybotCommands/lib/GetNumberOfDays.ts
+++ src/sassybotEventListeners/sassybotCommands/lib/GetNumberOfDays.ts
@@ -1,16 +1,6 @@
 import moment from 'moment';
 
 const getNumberOFDays = (firstSeenApi: string | Date | moment.Moment): number => {
-  const firstSeen = moment(firstSeenApi);
-  const firstPull = moment(new Date(2019, 9, 11, 23, 59, 59));
-  const beginningOfTime = moment(new Date(2019, 8, 2, 23, 59, 59));
-
-  if (firstSeen.isBefore(beginningOfTime)) {
-    return moment().diff(beginningOfTime, 'd');
-  } else if (firstSeen.isAfter(beginningOfTime) && firstSeen.isBefore(firstPull)) {
-    return moment().diff(beginningOfTime, 'd');
-  } else {
-    return moment().diff(firstSeen, 'd');
-  }
+  return moment().diff(moment(firstSeenApi), 'd');
 };
 export default getNumberOFDays;
