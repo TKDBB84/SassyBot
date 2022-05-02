0146859c29199470ce5788a1a2cb3d9041fe9a68
Jeffrey Campbell <jeffrey.campbell@autodesk.com>
2022-05-02 16:15:21 -0700

    all the intents

--- src/Sassybot.ts
+++ src/Sassybot.ts
@@ -116,12 +116,20 @@ export class Sassybot extends EventEmitter {
     intents.add(
       Intents.FLAGS.GUILDS,
       Intents.FLAGS.GUILD_MEMBERS,
+      Intents.FLAGS.GUILD_BANS,
       Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+      Intents.FLAGS.GUILD_INTEGRATIONS,
+      Intents.FLAGS.GUILD_WEBHOOKS,
+      Intents.FLAGS.GUILD_INVITES,
+      Intents.FLAGS.GUILD_VOICE_STATES,
       Intents.FLAGS.GUILD_PRESENCES,
       Intents.FLAGS.GUILD_MESSAGES,
       Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+      Intents.FLAGS.GUILD_MESSAGE_TYPING,
       Intents.FLAGS.DIRECT_MESSAGES,
       Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+      Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+      Intents.FLAGS.GUILD_SCHEDULED_EVENTS
     );
     this.discordClient = new Client({ intents, allowedMentions: { parse: ['users', 'roles'], repliedUser: true } });
     this.dbConnection = connection;
