bea0a7ff2508f0c64c7dfeccf9786c08c4377057
Jeffrey Campbell <jeffrey.campbell@autodesk.com>
2022-08-12 15:41:35 -0400

    clean up message

--- src/sassybotEventListeners/sassybotCommands/FactorioCommand.ts
+++ src/sassybotEventListeners/sassybotCommands/FactorioCommand.ts
@@ -149,13 +149,11 @@ export default class FactorioCommand extends SassybotCommand {
       const memoryUsage = stats.recent.memory.resource.usage;
       const memoryMax = stats.recent.memory.resource.limit;
       const memoryPercent = `${((memoryUsage / memoryMax) * 100).toFixed(1)}%`;
-      replyStats = `CPU: ${cpuUsage}%\nMemory: ${memoryUsage}/${memoryMax}MB (${memoryPercent})`;
+      replyStats = `CPU: ${cpuUsage} ¯\\_(ツ)_/¯\nMemory: ${memoryUsage}/${memoryMax}MB (${memoryPercent})`;
     }
 
     await message.reply(
-      `Server Status: \`${currentStatus}\`\nConnection String: \`${connectionString}${
-        replyStats ? `\n${replyStats}` : ''
-      }\``,
+      `Server Status: \`${currentStatus}\`\n` + `Connection String: \`${connectionString}\`\n${replyStats}`.trim(),
     );
   }
 
