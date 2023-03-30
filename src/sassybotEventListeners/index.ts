c493f748443f3243c84349dc307053580b912870
Jeffrey Campbell <jeffrey.campbell@autodesk.com>
2023-03-30 13:11:12 -0400

    remove new listener

--- src/sassybotEventListeners/index.ts
+++ src/sassybotEventListeners/index.ts
@@ -1,7 +1,12 @@
-import CoTNewMemberListener from './CoTNewMemberListener';
+// import CoTNewMemberListener from './CoTNewMemberListener';
 // import GamezzEyeListenerListener from './GamezzEyeListener';
 import QuoteListener from './QuoteListener';
 import VoiceLogListener from './VoiceLogListener';
 import sassyBotCommands from './sassybotCommands';
 
-export default [CoTNewMemberListener, QuoteListener, VoiceLogListener, ...sassyBotCommands];
+export default [
+  // CoTNewMemberListener,
+  QuoteListener,
+  VoiceLogListener,
+  ...sassyBotCommands,
+];
